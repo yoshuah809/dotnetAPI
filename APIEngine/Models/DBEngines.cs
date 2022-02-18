7621868f8f7d654f70647014ae01e4783f270895
Josue Garcia <yoshuah809@gmail.com>
2022-02-18 11:01:03 -0600

    created dbengine class model

---
+++ APIEngine/Models/DBEngines.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIEngine.Models
+{
+    public class DBEngines
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public int launched { get; set; }
+        public string developer { get; set; }
+    }
+}
